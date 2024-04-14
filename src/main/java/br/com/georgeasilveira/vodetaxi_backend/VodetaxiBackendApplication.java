3b046d2d05bbebf8b833e2ea378d356762916738
George Alves da Silveira <georgealvessilveira@outlook.com>
2024-04-14 11:46:22 -0300

    primeiro commit

---
+++ src/main/java/br/com/georgeasilveira/vodetaxi_backend/VodetaxiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.georgeasilveira.vodetaxi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodetaxiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodetaxiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/georgeasilveira/vodetaxi_backend/VodetaxiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.georgeasilveira.vodetaxi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodetaxiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
